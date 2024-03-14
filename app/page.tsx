d0a096d706beff67997c641aefa64d67cf4aff50
kodaneflash <jamesonmarcher@gmail.com>
2024-03-14 19:32:06 -0400

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,21 @@ import { Suspense } from 'react';
 export const runtime = 'edge';
 
 export const metadata = {
-  description: 'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
+  description: 'Our flawless designs, DMV grade printing materials, & advanced security features make our IDs stand out from the rest.',
   openGraph: {
-    type: 'website'
-  }
+    type: 'website',
+    url: 'https://idempire.us',
+    title: 'IDEmpire',
+    description: 'Your Trusted Vendor for Authentic Fake ID\'s',
+    images: [
+      {
+        url: 'https://idempire.us/images/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'IDEmpire',
+      },
+    ],
+  },
 };
 
 export default async function HomePage() {
