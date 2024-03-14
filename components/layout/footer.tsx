fefbfdcda3fd2a41730c14a9564f0f316e4978fa
kodaneflash <jamesonmarcher@gmail.com>
2024-03-14 19:07:53 -0400

    modified:   .env.example	modified:   components/layout/footer.tsx  not staged for commit:

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -59,7 +59,7 @@ export default async function Footer() {
           <p>Designed in California</p>
           <p className="md:ml-auto">
             <a href="https://vercel.com" className="text-black dark:text-white">
-              Crafted by ▲ Vercel
+              @IDEmpire
             </a>
           </p>
         </div>
